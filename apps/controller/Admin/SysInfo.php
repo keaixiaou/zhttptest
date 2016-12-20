586e7349b72f6688c207c3a7ef8ed923bb9e9351
huaerjie <286252682@qq.com>
2016-12-20 21:08:44 +0800

    删除测试model

--- apps/controller/Admin/SysInfo.php
+++ apps/controller/Admin/SysInfo.php
@@ -20,7 +20,7 @@ class SysInfo extends Common{
             '执行时间限制'=>ini_get('max_execution_time').'秒',
             '服务器时间'=>date("Y年n月j日 H:i:s"),
             '北京时间'=>gmdate("Y年n月j日 H:i:s",time()+8*3600),
-            '服务器域名/IP'=>$this->input->server['SERVER_NAME'].' [ '.gethostbyname($this->input->servier['SERVER_NAME']).' ]',
+            '服务器域名/IP'=>$this->input->server['SERVER_NAME'].' [ '.gethostbyname($this->input->server['SERVER_NAME']).' ]',
             '剩余空间'=>round((@disk_free_space(".")/(1024*1024)),2).'M',
             'register_globals'=>get_cfg_var("register_globals")=="1" ? "ON" : "OFF",
             'magic_quotes_gpc'=>(1===get_magic_quotes_gpc())?'YES':'NO',
